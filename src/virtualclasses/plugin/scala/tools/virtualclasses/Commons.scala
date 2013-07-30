8ca76c7c1b9866f9dcf32908011924fd0624811c
Manuel Weiel <manuel@weiel.eu>
2013-07-30 16:56:38 +0200

    Added virtual classes plugin

---
+++ src/virtualclasses/plugin/scala/tools/virtualclasses/Commons.scala
@@ -0,0 +1,129 @@
+package scala.tools.virtualclasses
+
+import scala.tools.nsc._
+import scala.tools.nsc.symtab.Flags._
+import scala.tools.nsc.transform._
+
+/**
+
+  */
+trait Commons {
+
+  val global : Global
+
+  import global._
+
+
+  //Prefixes for synthesized symbol names:
+  //factories
+  val FACTORYPREFIX = "VC_NEW$"
+  //workertraits
+  val TRAITPREFIX = "VC_TRAIT$"
+  //workertrait implementations
+  val CCLASSPREFIX = "VC_FIX$"
+  //final bindings
+  val FINALPREFIX = "VC_FINAL$"
+  //this is here for debugging purposes, the abstract types will inherit the virtual class' name eventually
+  val ABSTPEPREFIX = "VC_T$"
+  //dito for initial bindings
+  val INITPREFIX = "VC_INIT$"
+
+  def isVCAbstractType(sym : Symbol) : Boolean = {
+    //TODO find an alternative way to determine this (add new flags?)
+    sym.name.startsWith(ABSTPEPREFIX)
+  }
+
+  def finalBindingName(sym : Symbol) : TypeName = {
+    if (sym.name.startsWith(FINALPREFIX))
+      return sym.name.toTypeName
+
+    val prefixToStrip =
+      if(sym.name.startsWith(INITPREFIX))
+        INITPREFIX
+      else ""
+
+    newTypeName(FINALPREFIX+(sym.name.toString().stripPrefix(prefixToStrip)))
+  }
+
+  def isInitialBinding(sym : Symbol) : Boolean = {
+    //TODO find an alternative way to determine this (add new flags?)
+    sym.isAbstractClass && sym.info.members.exists(x => x.isTrait && x.name.startsWith(TRAITPREFIX))
+  }
+
+  def containsInitialBindings(sym : Symbol) : Boolean = {
+    sym.info.members.exists(isInitialBinding)
+  }
+
+  def stripAllPrefixes(name: Name) = {
+    val prefixToStrip =
+      if(name.startsWith(ABSTPEPREFIX))
+        ABSTPEPREFIX
+      else if (name.startsWith(FACTORYPREFIX))
+        FACTORYPREFIX
+      else if (name.startsWith(CCLASSPREFIX))
+        CCLASSPREFIX
+      else if (name.startsWith(FINALPREFIX))
+        FINALPREFIX
+      else ""
+    newTypeName(name.toString.stripPrefix(prefixToStrip))
+  }
+
+  def factoryName(clazz: Symbol) = { //TODO make method more robust
+    val prefixToStrip =
+    if(clazz.name.startsWith(ABSTPEPREFIX))
+      ABSTPEPREFIX
+    else if (clazz.name.startsWith(TRAITPREFIX))
+      TRAITPREFIX
+    else if (clazz.name.startsWith(CCLASSPREFIX))
+      CCLASSPREFIX
+    else ""
+
+    newTermName(FACTORYPREFIX+(clazz.name.toString().stripPrefix(prefixToStrip)))
+  }
+
+  def workerTraitName(sym: Symbol) : TypeName =
+    workerTraitName(sym.name, sym.owner.name)
+
+  def workerTraitName(name: Name, parent: Name) : TypeName = {
+    if(name.startsWith(TRAITPREFIX))
+      return name.toTypeName
+
+    val prefixToStrip =
+      if(name.startsWith(ABSTPEPREFIX))
+        ABSTPEPREFIX
+      else if (name.startsWith(FACTORYPREFIX))
+        FACTORYPREFIX
+      else if (name.startsWith(CCLASSPREFIX))
+        CCLASSPREFIX
+      else ""
+
+    val parentprefixToStrip =
+      if(parent.startsWith(ABSTPEPREFIX))
+        ABSTPEPREFIX
+      else if (parent.startsWith(FACTORYPREFIX))
+        FACTORYPREFIX
+      else if (parent.startsWith(CCLASSPREFIX))
+        CCLASSPREFIX
+      else if (parent.startsWith(FINALPREFIX))
+        FINALPREFIX
+      else ""
+
+    newTypeName(TRAITPREFIX + parent.toString.stripPrefix(parentprefixToStrip) + "$" +(name.toString().stripPrefix(prefixToStrip)))
+  }
+
+  def concreteClassName(sym : Symbol) : TypeName = {
+    if (sym.name.startsWith(TRAITPREFIX))
+      return sym.name.toTypeName
+
+    val prefixToStrip =
+      if(sym.name.startsWith(ABSTPEPREFIX))
+        ABSTPEPREFIX
+      else if (sym.name.startsWith(FACTORYPREFIX))
+        FACTORYPREFIX
+      else if (sym.name.startsWith(TRAITPREFIX))
+        TRAITPREFIX
+      else ""
+
+    newTypeName(CCLASSPREFIX+(sym.name.toString().stripPrefix(prefixToStrip)))
+  }
+}
---
+++ src/virtualclasses/plugin/scala/tools/virtualclasses/VCFinalBindingsTransform.scala
@@ -0,0 +1,166 @@
+package scala.tools.virtualclasses
+
+import scala.tools.nsc._
+import scala.tools.nsc.symtab.Flags._
+import scala.tools.nsc.plugins.PluginComponent
+import scala.tools.nsc.transform._
+
+/**
+ *
+ */
+
+abstract class VCFinalBindingsTransform(val global: Global) extends PluginComponent with Transform
+  with TypingTransformers with InfoTransform with Commons {
+  import global._
+
+  override val phaseName = "vc_finalbindings"
+
+  override def transformInfo(sym: Symbol, tpe: Type) =
+    infoTransformer(tpe)
+
+  private object infoTransformer extends TypeMap {
+    def apply(tpe: Type) = {
+      val tpe0 = mapOver(tpe)
+      tpe0 match {
+        case ClassInfoType(parents, decls0, clazz) if (containsInitialBindings(clazz)) =>
+
+          val decls = newScope
+          def enter(sym: Symbol) =
+            /*atPhase(ownPhase.next) {*/ decls.enter(sym) /*}*/
+          for (m <- decls0) {
+            enter(m)
+            if (isInitialBinding(m))
+              enter(mkFinalBindingSym(m))
+          }
+
+          ClassInfoType(parents map this, decls, clazz)
+
+        case x => x
+      }
+    }
+  }
+
+  def mkFinalBindingSym(initBinding: Symbol): Symbol = {
+    val fbname = finalBindingName(initBinding)
+    val fbsym = initBinding.owner.enclClass.newClass(fbname, initBinding.pos).setFlag(SYNTHETIC)
+    //val parents = List(definitions.ObjectClass.tpe, initBinding.tpe)
+    val parents = List(initBinding.tpe)
+    val scope = newScope
+    fbsym setInfo ClassInfoType(parents, scope, fbsym)
+
+    //for every abstract type, create alias and provide factory definition
+    for (abstpe <- initBinding.info.decls.filter(isVCAbstractType)) {
+      //alias type
+      val absTpeBinding = abstpe.cloneSymbol(fbsym)
+      val workerTrait = initBinding.info.decl(workerTraitName(abstpe))
+      absTpeBinding setInfo workerTrait.tpe.substThis(initBinding, ThisType(fbsym))
+      absTpeBinding.resetFlag(DEFERRED)
+      scope enter absTpeBinding
+
+      //factory
+      val factory = initBinding.info.decl(factoryName(abstpe)).cloneSymbol(fbsym)
+      factory.resetFlag(DEFERRED)
+      factory setInfo factory.tpe.substThis(initBinding, ThisType(fbsym))
+      scope enter factory
+    }
+
+    fbsym
+  }
+
+  def newTransformer(unit: CompilationUnit) = new Transformer(unit)
+
+  class Transformer(unit: CompilationUnit) extends TypingTransformer(unit) {
+
+    protected def finalBindingSym(vc: Symbol): Symbol = {
+      atPhase(ownPhase.next) {
+        vc.owner.enclClass.info.member(finalBindingName(vc))
+      }
+    }
+
+    protected def mkConcreteClassSym(factory: Symbol, initBinding: Symbol) = {
+      val workerTraitSym = factory.enclClass.info.member(workerTraitName(factory))
+      val cclazz = factory.newClass(factory.pos, concreteClassName(factory))
+        .setFlag(SYNTHETIC)
+        .setAnnotations(initBinding.annotations)
+      val parentClass = (workerTraitSym.info.baseClasses.dropWhile(_.isTrait)).head.tpe
+      val mixins = (workerTraitSym.info.baseClasses.filter(_.isTrait)).distinct.reverse.map(_.tpe)
+      val parents = (parentClass :: mixins).map(_.substThis(initBinding, ThisType(factory.enclClass)).substSym(workerTraitSym.typeParams, factory.typeParams)) //TODO is this sufficient?
+
+      cclazz setInfo ClassInfoType(parents, newScope, cclazz)
+      cclazz
+    }
+
+    protected def mkFactoryDefDef(factory: Symbol, initBinding: Symbol): Tree = {
+      val cclazzSym = mkConcreteClassSym(factory, initBinding)
+      val cclazzDef = ClassDef(cclazzSym, NoMods, List(List()), List(List()), List(), factory.enclClass.pos.focus)
+
+      val args = factory.paramss map (_.map(Ident)) //TODO clone or not?
+      val body = Block(List(cclazzDef),
+        TypeApply(Select(New(TypeTree(cclazzSym.tpe.substSym(cclazzSym.typeParams, factory.typeParams)), args), definitions.Any_asInstanceOf),
+          List(TypeTree(factory.info.resultType))))
+
+      atPos(factory.enclClass.pos) {
+        DefDef(factory, Modifiers(factory.flags), body)
+      }
+    }
+
+    protected def mkFinalBinding(initBinding: Symbol): Tree = {
+      val finalBinding = finalBindingSym(initBinding)
+      val tpeBindings = finalBinding.info.members.toList.filter(isVCAbstractType) //TODO should we distinguish between abstract type and its concrete binding? If so, use another predicate
+      val factories = finalBinding.info.members.toList.filter(_.name.startsWith(FACTORYPREFIX))
+
+      def mkAbsTpeBinding(tpeSym: Symbol): Tree = {
+        val workerTrait = finalBinding.info.member(workerTraitName(tpeSym))
+        TypeDef(tpeSym, TypeTree(workerTrait.tpe.substThis(initBinding, ThisType(finalBinding))))
+      }
+
+      val body: List[Tree] = (tpeBindings map mkAbsTpeBinding) ::: (factories map (mkFactoryDefDef(_, initBinding)))
+
+      val classDef = ClassDef(finalBinding, Modifiers(0), List(List()), List(List()), body, initBinding.pos) //TODO which modifiers?
+
+      localTyper.typed {
+        atPos(initBinding.pos) {
+          classDef
+        }
+      }
+    }
+
+    override def transformStats(stats: List[Tree], exprOwner: Symbol): List[Tree] = {
+      stats flatMap transformStat
+    }
+
+    protected def transformStat(tree: Tree): List[Tree] = {
+      val sym = tree.symbol
+      tree match {
+        case cd @ ClassDef(mods, name, tparams, templ) if (isInitialBinding(sym)) =>
+          val fbtree = mkFinalBinding(sym)
+          val newCd = ClassDef(mods, name, tparams, transform(templ).asInstanceOf[Template])
+          newCd.copyAttrs(cd)
+          List(newCd, fbtree)
+        case _ => List(transform(tree))
+      }
+    }
+
+    override def transform(tree0: Tree): Tree = {
+      val tree = super.transform(tree0)
+
+      postTransform(tree setType atPhase(ownPhase) {
+        infoTransformer(tree.tpe)
+      })
+    }
+
+    protected def postTransform(tree: Tree): Tree = {
+      tree match {
+        //replace new calls to family class with the final implementation of the family class
+        case app @ Apply(sel @ Select(New(tpt), nme.CONSTRUCTOR), args) if (isInitialBinding(app.symbol.owner) && app.symbol.isConstructor) =>
+          localTyper.typed {
+            atPos(tree.pos) {
+              Apply(Select(New(finalBindingSym(app.symbol.owner)), nme.CONSTRUCTOR), args)
+            }
+          }
+        case t => t
+      }
+    }
+  }
+
+}
